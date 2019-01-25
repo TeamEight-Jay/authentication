fdbce872f42b5fbbba6590fdb58d8954850e5f7a
jayjoshi <jay.joshi@coviam.com>
2019-01-25 14:39:45 +0530

    minor fix

--- src/main/java/com/teamfive/authentication/controller/SignUpController.java
+++ src/main/java/com/teamfive/authentication/controller/SignUpController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.regex.Pattern;
+
 @RestController
 public class SignUpController {
     @Autowired
@@ -17,6 +19,14 @@ public class SignUpController {
     @PostMapping(value = "auth/signup")
     public ResponseDto add(@RequestBody UserDto userDto) {
         // System.out.println(userDto);
+
+        if(userDto==null) return new ResponseDto("FAILED","MALFORMED INPUTS #E1");
+        if(userDto.getEmailId()==null) return new ResponseDto("FAILED","MALFORMED INPUTS #E2");
+        if(userDto.getFirstName()==null) return new ResponseDto("FAILED","MALFORMED INPUTS #E3");
+        if(userDto.getLastName()==null) return new ResponseDto("FAILED","MALFORMED INPUTS #E4");
+        if(userDto.getPassword()==null) return new ResponseDto("FAILED","MALFORMED INPUTS #E5");
+        if(userDto.getPhoneNumber()==null) return new ResponseDto("FAILED","MALFORMED INPUTS #E6");
+
         User userExists = userService.find(userDto.getEmailId());
         if(userExists != null){
             return new ResponseDto("FAILED","User already exists");
